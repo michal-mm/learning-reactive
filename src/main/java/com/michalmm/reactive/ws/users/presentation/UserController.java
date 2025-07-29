6720a147e0f00b7997ce3cb6ec6bdb224ed7a695
michal-mm <michal@Magic.local>
2025-07-29 14:42:19 +0200

    basic post method handler and user request handler bean class

--- src/main/java/com/michalmm/reactive/ws/users/presentation/UserController.java
+++ src/main/java/com/michalmm/reactive/ws/users/presentation/UserController.java
@@ -1,10 +1,19 @@
 package com.michalmm.reactive.ws.users.presentation;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+	@PostMapping
+	public void createUser(@RequestBody Mono<CreateUserRequest> createUserRequest) {
+		
+	}
 
 }
