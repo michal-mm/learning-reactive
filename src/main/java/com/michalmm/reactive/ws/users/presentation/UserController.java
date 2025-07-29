373b25aad1e964751ec1d5bae7ac723c2f810402
michal-mm <michal@Mac.lan>
2025-07-29 15:34:23 +0200

    functional programming - creating userRest from request body and then creating response entity object that has userREst inside; it returns CREATED 201 status and the location URL

--- src/main/java/com/michalmm/reactive/ws/users/presentation/UserController.java
+++ src/main/java/com/michalmm/reactive/ws/users/presentation/UserController.java
@@ -1,8 +1,10 @@
 package com.michalmm.reactive.ws.users.presentation;
 
+import java.net.URI;
 import java.util.UUID;
-import java.util.concurrent.locks.ReadWriteLock;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,13 +18,16 @@ import reactor.core.publisher.Mono;
 public class UserController {
 
 	@PostMapping
-	public Mono<UserRest> createUser(@RequestBody @Valid Mono<CreateUserRequest> createUserRequest) {
+//	@ResponseStatus(HttpStatus.CREATED)
+	public Mono<ResponseEntity<UserRest>> createUser(@RequestBody @Valid Mono<CreateUserRequest> createUserRequest) {
 
 		return createUserRequest.map(request -> new UserRest(UUID.randomUUID(),
 													request.getFirstName(),
 													request.getLastName(),
-													request.getEmail())
-		);
+													request.getEmail()))
+				.map( userRest -> ResponseEntity.status(HttpStatus.CREATED)
+						.location(URI.create("/users/" + userRest.getId()) )
+						.body(userRest) );
 	}
 
 }
