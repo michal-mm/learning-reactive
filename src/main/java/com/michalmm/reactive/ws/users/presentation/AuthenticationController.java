fd5157673152574a86ee1ffccfe6d57dc21a3083
michal-mm <michal@Mac.lan>
2025-07-30 12:02:23 +0200

    authentication implementation

--- src/main/java/com/michalmm/reactive/ws/users/presentation/AuthenticationController.java
+++ src/main/java/com/michalmm/reactive/ws/users/presentation/AuthenticationController.java
@@ -1,19 +1,40 @@
 package com.michalmm.reactive.ws.users.presentation;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.michalmm.reactive.ws.users.presentation.model.AuthenticationRequest;
+import com.michalmm.reactive.ws.users.service.AuthenticationService;
 
 import reactor.core.publisher.Mono;
 
 @RestController
 public class AuthenticationController {
+	
+	private final AuthenticationService authenticationService;
+	
+	
+
+	public AuthenticationController(AuthenticationService authenticationService) {
+		super();
+		this.authenticationService = authenticationService;
+	}
+
+
 
 	@PostMapping("/login")
-	public Mono<ResponseEntity<Void>> login(@RequestBody Mono<AuthenticationRequest> authenticationRequest) {
-		return Mono.just(ResponseEntity.ok().build());
+	public Mono<ResponseEntity<Void>> login(@RequestBody Mono<AuthenticationRequest> authenticationRequestMono) {
+		return authenticationRequestMono
+				.flatMap(authenticationRequest -> 
+					authenticationService.authenticate(authenticationRequest.getEmail(), 
+											authenticationRequest.getPassword()))
+				.map(authenticationResultMap -> ResponseEntity.ok()
+						.header(HttpHeaders.AUTHORIZATION, "Bearer "
+								+ authenticationResultMap.get("token"))
+						.header("UserId", authenticationResultMap.get("userId"))
+						.build());
 	}
 }
