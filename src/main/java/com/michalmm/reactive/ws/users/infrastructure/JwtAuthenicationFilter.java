24c08bdc433200c526f89ea2fa32fa30284b839d
michal-mm <michal@Mac.lan>
2025-07-30 13:39:33 +0200

    adding implementation for authentication by using token extraction and handling invalid tokens

--- src/main/java/com/michalmm/reactive/ws/users/infrastructure/JwtAuthenicationFilter.java
+++ src/main/java/com/michalmm/reactive/ws/users/infrastructure/JwtAuthenicationFilter.java
@@ -1,6 +1,10 @@
 package com.michalmm.reactive.ws.users.infrastructure;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
@@ -8,6 +12,7 @@ import org.springframework.web.server.WebFilterChain;
 
 import com.michalmm.reactive.ws.users.service.JwtService;
 
+import io.jsonwebtoken.lang.Collections;
 import reactor.core.publisher.Mono;
 
 public class JwtAuthenicationFilter implements WebFilter {
@@ -27,10 +32,31 @@ public class JwtAuthenicationFilter implements WebFilter {
 		
 		String token = extractToken(exchange);
 		
-		return chain.filter(exchange);
+		if (token == null) return chain.filter(exchange);
+		
+		return validateToken(token)
+				.flatMap(isValid -> isValid ? authenticateAndContinue(token, exchange, chain)
+											: handleInvalidToken(exchange));
 	}
 	
 	
+	private Mono<Void> handleInvalidToken(ServerWebExchange exchange) {
+		exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+		return exchange.getResponse().setComplete();
+	}
+
+
+	private Mono<Void> authenticateAndContinue(String token, ServerWebExchange exchange, WebFilterChain chain) {
+		return Mono.just("extractTokenSubject()")
+				.flatMap(subject -> {
+					Authentication auth = new UsernamePasswordAuthenticationToken(subject, null, 
+							Collections.emptyList());
+					return chain.filter(exchange)
+							.contextWrite(ReactiveSecurityContextHolder.withAuthentication(auth));
+				});
+	}
+
+
 	private String extractToken(ServerWebExchange exchange) {
 		String authorizarionHeader = exchange.getRequest()
 									.getHeaders()
