0de4c2b382df99e27dbb5c0ee24a5ebad00f5b23
michal-mm <michal@Mac.lan>
2025-07-30 15:10:01 +0200

    adding filter logic into security filter chain and add stateless setting to not pass context across sessions by using NoOpServerSecurityContextRepository

--- src/main/java/com/michalmm/reactive/ws/users/infrastructure/WebSecurity.java
+++ src/main/java/com/michalmm/reactive/ws/users/infrastructure/WebSecurity.java
@@ -5,10 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
+
+import com.michalmm.reactive.ws.users.service.JwtService;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -16,7 +20,10 @@ public class WebSecurity {
 
 	@Bean
 	public SecurityWebFilterChain httpSecurityFilterChain(ServerHttpSecurity http,
-									ReactiveAuthenticationManager authenticationManager) {
+									ReactiveAuthenticationManager authenticationManager,
+									JwtService jwtService) {
+		JwtAuthenicationFilter jwtAuthenticationFilter = new JwtAuthenicationFilter(jwtService);
+		
 		return http
 				.authorizeExchange(exchanges -> exchanges
 						.pathMatchers(HttpMethod.POST, "/users").permitAll()
@@ -28,6 +35,8 @@ public class WebSecurity {
 				.csrf(ServerHttpSecurity.CsrfSpec::disable)
 				.httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
 				.authenticationManager(authenticationManager)
+				.addFilterAt(jwtAuthenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+				.securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
 				.build();
 	}
 	
