93211deb8c9ff1a2a6e26e24f41e649001e75175
michal-mm <michal@Mac.lan>
2025-07-30 09:21:57 +0200

    adding method defintion for fetching all items from suers table using pagination

--- src/main/java/com/michalmm/reactive/ws/users/data/UserRepository.java
+++ src/main/java/com/michalmm/reactive/ws/users/data/UserRepository.java
@@ -2,8 +2,12 @@ package com.michalmm.reactive.ws.users.data;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+import reactor.core.publisher.Flux;
 
 public interface UserRepository extends ReactiveCrudRepository<UserEntity, UUID> {
 
+	public Flux<UserEntity> findAllBy(Pageable pageable);
 }
